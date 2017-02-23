a67eabfa0e57de9ba81723033b38e6312f05d660
gl00my-c0der <afif.mahmud.temp@gmail.com>
2017-02-23 21:10:42 +0600

    Update Assignment 2.cpp

--- Assignment 2.cpp
+++ Assignment 2.cpp
@@ -7,13 +7,20 @@ int main()
     cout<< "This program converts meter into feet"<<endl;
     cout<< ""<<endl;
 
-    int length;
+    int length;                                          
+    //you did not use the declared variable length anywhere, so this line is useless
+    
     cout<< "Enter a value of length in meter"<<endl;
 
     float meter;
+    //declare variables all together if you have just one code body
+    //since you did not use int length, float meter isn't conflicting. But if you did int variables wouldn't be able to handle float inputs.
+    
     cin>> meter;
 
     float feet = meter * 3.28084;
+    //declare feet as a float before defining/use it, preferably in the same line in float meter.
+    //example: float meter, feet;
 
     cout<< "Length in feet = " <<feet<< endl;
 
